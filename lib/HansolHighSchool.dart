e161405ffec522402ff9d175433f423da6cb62cb
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2023-12-17 11:09:00 +0900

    Migration

--- lib/HansolHighSchool.dart
+++ lib/HansolHighSchool.dart
@@ -2,15 +2,11 @@ import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:hansol_high_school/API/MealDataApi.dart';
 import 'package:hansol_high_school/Screens/MainScreens/HomeScreen.dart';
 import 'package:hansol_high_school/Screens/MainScreens/MealScreen.dart';
 import 'package:hansol_high_school/Screens/MainScreens/NoticeScreen.dart';
-import 'package:intl/intl.dart';
-import 'package:timezone/timezone.dart' as tz;
 import 'package:hansol_high_school/Notification/NotificationManager.dart';
-import 'Firebase/firebase_options.dart';
-import 'Notification/ScheduledNotification.dart';
+import 'package:hansol_high_school/Firebase/firebase_options.dart';
 
 Future<void> main() async {
   runApp(const HansolHighSchool());
