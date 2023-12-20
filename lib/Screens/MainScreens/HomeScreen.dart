5516325e8363f0504f2816a05436968ae8fcfcf8
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2023-12-20 14:36:46 +0900

    Migration

--- lib/Screens/MainScreens/HomeScreen.dart
+++ lib/Screens/MainScreens/HomeScreen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hansol_high_school/API/MealDataApi.dart';
-import 'package:hansol_high_school/Widgets/MealWidgets/MealCard.dart';
 
 class HansolHighSchool extends StatelessWidget {
   @override
@@ -18,31 +16,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 20.0,
-        ),
-        MealCard(
-          mealType: "조식",
-          mealKcal: MealDataApi.getMeal(
-            date: DateTime.now(),
-            mealType: MealDataApi.BREAKFAST,
-            type: MealDataApi.CALORIE,
-          ),
-          mealInfo: MealDataApi.getMeal(
-            date: DateTime.now(),
-            mealType: MealDataApi.BREAKFAST,
-            type: MealDataApi.MENU,
-          ),
-        ),
-      ],
-    );
+    return Center();
   }
 }
