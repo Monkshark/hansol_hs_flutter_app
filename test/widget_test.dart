a5ca97cc1868cc4b19a7de099a26eff8d9562c59
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2023-12-20 23:32:54 +0900

    Migration

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hansol_high_school/HansolHighSchool.dart';
+import 'package:hansol_high_school/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
