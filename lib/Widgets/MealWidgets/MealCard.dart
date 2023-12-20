27e88a292f3f6ed707bc9ca3acf83b9aa2399617
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2023-12-20 23:26:24 +0900

    Migration

--- lib/Widgets/MealWidgets/MealCard.dart
+++ lib/Widgets/MealWidgets/MealCard.dart
@@ -8,6 +8,6 @@ class MealCard extends StatefulWidget {
 class _MealCardState extends State<MealCard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
