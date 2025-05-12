11861b2e547efa160c28103c1c8aa8d306ab499d
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2025-05-12 10:43:15 +0900

    Migration

--- lib/Data/subject.dart
+++ lib/Data/subject.dart
@@ -2,8 +2,16 @@ class Subject {
   final String subjectName;
   final int subjectClass;
 
-  Subject({
-    required this.subjectName,
-    required this.subjectClass,
-  });
+  Subject({required this.subjectName, required this.subjectClass});
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Subject &&
+          runtimeType == other.runtimeType &&
+          subjectName == other.subjectName &&
+          subjectClass == other.subjectClass;
+
+  @override
+  int get hashCode => subjectName.hashCode ^ subjectClass.hashCode;
 }
