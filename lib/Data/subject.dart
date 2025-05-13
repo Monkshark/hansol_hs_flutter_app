439d0fd45f7b6556acce32ffa9010d03dcd9933d
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2025-05-13 18:50:20 +0900

    Migration

--- lib/Data/subject.dart
+++ lib/Data/subject.dart
@@ -1,17 +1,27 @@
 class Subject {
   final String subjectName;
   final int subjectClass;
+  final String? category;
+  final bool isOriginal;
 
-  Subject({required this.subjectName, required this.subjectClass});
+  Subject({
+    required this.subjectName,
+    required this.subjectClass,
+    this.category,
+    this.isOriginal = false,
+  });
 
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is Subject &&
-          runtimeType == other.runtimeType &&
-          subjectName == other.subjectName &&
-          subjectClass == other.subjectClass;
+  Map<String, dynamic> toJson() => {
+        'subjectName': subjectName,
+        'subjectClass': subjectClass,
+        if (category != null) 'category': category,
+        'isOriginal': isOriginal,
+      };
 
-  @override
-  int get hashCode => subjectName.hashCode ^ subjectClass.hashCode;
+  factory Subject.fromJson(Map<String, dynamic> json) => Subject(
+        subjectName: json['subjectName'],
+        subjectClass: json['subjectClass'],
+        category: json['category'],
+        isOriginal: json['isOriginal'] ?? false,
+      );
 }
