760deb2cd220c0eed254a16ee23656a53f608360
Justin choo <manner0814@naver.com>
2026-04-17 16:09:37 +0900

    chore: 불필요한 주석 제거 + prefer_const_declarations 린트 수정
    - lib/, android/, ios/, docs/ 전체에서 // 주석 149개 라인 제거, 인라인 주석 18개 정리
    - 생성 파일(*.g.dart, *.freezed.dart, l10n/*) 및 pragma(// ignore:) 보존
    - main.dart prefer_const_declarations info 이슈 수정

--- lib/data/api_strings.dart
+++ lib/data/api_strings.dart
@@ -3,16 +3,13 @@
 class ApiStrings {
   ApiStrings._();
 
-  // meal
   static const mealNoData = '급식 정보가 없습니다.';
   static const mealNoDataLegacy = '급식 정보가 없습니다';
   static const mealNoInternet = '식단 정보를 확인하려면 인터넷에 연결하세요';
 
-  // timetable
   static const timetableNoInternet = '시간표를 확인하려면 인터넷에 연결하세요';
   static const timetableNoData = '정보 없음';
 
-  // notice
   static const noticeNoInternet = '학사일정을 확인하려면 인터넷에 연결하세요';
   static const noticeNoData = '학사일정이 없습니다';
 }
